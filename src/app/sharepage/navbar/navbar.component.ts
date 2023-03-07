ede263e35292b87358c71b25aa4a6b85ea453b19
BORITH-Monorom <borithmonorom@gmail.com>
2023-03-07 17:06:52 +0700

    update

--- src/app/sharepage/navbar/navbar.component.ts
+++ src/app/sharepage/navbar/navbar.component.ts
@@ -3,89 +3,72 @@ import {MatDialog} from '@angular/material/dialog';
 import { DialogExampleComponent } from './dialog-example/dialog-example.component';
 import {FormControl, Validators} from '@angular/forms';
 import { Observable } from 'rxjs';
-interface Animal {
-  name: string;
-  sound: string;
-}
-interface FLAG {
-  value: string;
-  viewValue: string;
-}
 
-export interface Asean{
-  flag: string;
-  country: string;
-}
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 
-
 export class NavbarComponent {
-
+public ImgShow:string = 'https://www.hrincjobs.com/kh.45d67420b01d8d8c.svg';
+public ImgCam = 'https://www.hrincjobs.com/kh.45d67420b01d8d8c.svg';
+public ImgLa = 'https://www.hrincjobs.com/la.27d2ef9c1e67e9d7.svg';
+public ImgMm = 'https://www.hrincjobs.com/mm.adaa2111ae2837d0.svg';
+public ImgAsean = 'https://www.hrincjobs.com/assets/images/asean.svg';
+public country =  "Cambodia";
+cam(){
+  this.country = "Cambodia";
+  this.ImgShow = this.ImgCam;
+}
+la(){
+  this.country = "Lao";
+  this.ImgShow = this.ImgLa;
+}
+mm(){
+  this.country = "Myanmar";
+  this.ImgShow = this.ImgMm;
+}
+asean(){
+  this.country = "Asean";
+  this.ImgShow = this.ImgAsean;
+}
   constructor(public dialog: MatDialog){}
   flag:any=['cambodia','lao','Myanmar'];
   openDialog(){
   this.dialog.open(DialogExampleComponent);
   }
-
-//language
-langs:boolean = true;
-lang = "Asean";
-laos:boolean = true;
-mys:boolean = true;
-la:{} | undefined;
-cam(){
-this.lang = "Cambodia";
-}
-lao(){
-  this.lang = "Lao"
-}
-my(){
-  this.lang = "Myanmar";
-}
-
-// end language
-
   //toggle
   expended = false;
   toggle(){
     this.expended = !this.expended;
   }
+  public employer = "employer";
+  onchange(){
+  this.employer = "Job Seeker";
+  }
+  isSticky: boolean = false;
 
-public employer = "employer";
+// sticky navbar that hides when scrolling up in TypeScript:
+onScroll(): void {
+  let currentPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-onchange(){
-this.employer = "Job Seeker";
+  if (currentPosition > 50) {
+    this.isSticky = true;
+  } else {
+    this.isSticky = false;
+  }
 }
 
-// isOpen = false;
-ShowMessage(){
-this.inStock = !this.inStock;
-}
-  public inStock = true;
-  public availableSoon = true;
-
-
-
-
-
-
-
-  aseans: Asean[] = [
-    {
-      country: 'Arkansas',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
-    },
-    {
-      country: 'Arkansas',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
-    },
-
-  ];
-
+ngOnInit() {
+  window.addEventListener('scroll', this.onScroll, { passive: true });
 }
 
+ngOnDestroy() {
+  window.removeEventListener('scroll', this.onScroll);
+}
+// sticky navbar that hides when scrolling up in TypeScript:
+
+
+}
 
